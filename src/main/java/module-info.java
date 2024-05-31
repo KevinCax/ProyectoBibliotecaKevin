1adfa8e14e6ee38a1ba624d1fec02dd29f167553
KevinCax <caxkevin86@gmail.com>
2024-05-30 22:55:06 -0600

    Corrigiendo errores del jar, se agrega .jar version 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,4 @@ module kevincax.proyectobiblioteca {
 
     opens kevincax.proyectobiblioteca to javafx.fxml;
     exports kevincax.proyectobiblioteca;
-    exports kevincax.proyectobiblioteca.Controllers.BaseDeDatos;
-    exports kevincax.proyectobiblioteca.Controllers.Controladores;
 }
